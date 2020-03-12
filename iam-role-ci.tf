fcd20ae6258fc2bfd15d406e85bbae8ac65b4ac5
Arthur Diniz <arthurbdiniz@gmail.com>
2020-03-12 17:21:01 +1100

    Fix code format issues
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- iam-role-ci.tf
+++ iam-role-ci.tf
@@ -16,19 +16,18 @@ data "aws_iam_policy_document" "assume_role_ci_deploy_ec2" {
 }
 
 resource "aws_iam_role" "ci_deploy_ec2" {
-  name                 = "ci-deploy-ec2"
-  assume_role_policy   = "${data.aws_iam_policy_document.assume_role_ci_deploy_ec2.json}"
- }
+  name               = "ci-deploy-ec2"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_ci_deploy_ec2.json}"
+}
 
 resource "aws_iam_instance_profile" "ci_deploy_ec2" {
-  name  = "ci-deploy"
-  role  = "${aws_iam_role.ci_deploy_ec2.name}"
+  name = "ci-deploy"
+  role = "${aws_iam_role.ci_deploy_ec2.name}"
 }
 
 resource "aws_iam_user" "ci_deploy" {
-  name  = "ci-deploy"
+  name = "ci-deploy"
 }
-
 
 data "aws_iam_policy_document" "ci_deploy_policy" {
   statement {
@@ -47,4 +46,3 @@ resource "aws_iam_user_policy_attachment" "ci_admin" {
   user       = "${aws_iam_user.ci_deploy.name}"
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
-
